a3bc652df350666b7bf32022e311f5b962df3a8d
jungtravor <jungtravor@gmail.com>
2023-01-27 01:00:29 +0800

    first commit

---
+++ settings/i18n/support.js
@@ -0,0 +1,18 @@
+
+function jqi18nLoad(language){
+    let props = {
+        titles: ['dashboard', 'configuration']
+    }
+    jQuery.i18n.properties({
+        name: 'description', 
+        path: 'settings/i18n/', 
+        mode: 'both',
+        language,
+        async: true,
+        callback: function() {
+            jQuery.i18n.prop('titles_dashboard')
+            alert(titles_dashboard)
+        }
+    });
+}
+
