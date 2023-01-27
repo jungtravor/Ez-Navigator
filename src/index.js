86dc38d00a4efcd73f0e35be0774e857a99d8b26
jungtravor <jungtravor@gmail.com>
2023-01-28 00:47:39 +0800

    t

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@ function jqi18nLoad(language){
     let props = {
         titles: ['dashboard', 'configuration']
     }
-    jQuery.i18n.properties({
-        name: 'description', 
-        path: 'settings/i18n/', 
+    $.i18n.properties({
+        name: 'description',
+        path: 'settings/i18n/',
         mode: 'both',
         language,
         async: true,
         callback: function() {
-            jQuery.i18n.prop('titles_dashboard')
-            alert(titles_dashboard)
+            console.log($(this).data("locale"))
+            $(this).html($.i18n.prop($(this).data("locale")));
+            $.i18n.prop('titles_dashboard')
         }
     });
 }
